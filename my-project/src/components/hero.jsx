d58f28536c663fec46511aa650699b7a7acbd19f
Fikunmi01 <93198530+Fikunmi01@users.noreply.github.com>
2023-08-16 07:49:21 +0100

    Update:Hero completed

--- my-project/src/components/hero.jsx
+++ my-project/src/components/hero.jsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
   return (
     <>
-      <div className="flex px-28 py-6">
+      <div className="flex pl-28 py-6">
         <div>
           <span>
             <h1 className="text-6xl text-darkBrown font-serif font-medium mb-4">
@@ -14,35 +14,36 @@ export const Hero = () => {
               businesses to manage staff, pay salaries, bills and relevant taxes
               all at once.
             </p>
-            <button className="px-9 py-4 bg-darkGreen font-sans text-base mb-16 font-medium text-white rounded-full shadow-2xl">
+            <button className="px-9 py-4 bg-darkGreen font-sans text-base mb-12 font-medium text-white rounded-full shadow-2xl">
               Start Using Free, Forever
             </button>
           </span>
           <span>
-            <h2 className="font-sans text-lg font-normal text-dblack">
+            <h2 className="font-sans text-lg font-normal mb-4 text-dblack">
               Download the Eazipay App
             </h2>
-            <div className="flex">
+            <div className="flex gap-6 text-darkBrown font-sans">
               <div className="">
-                <div>
-                  <img src="/assets/ellipse.png" alt="" />
-                  <img src="" alt="" />
-                </div>
-                <div className="bg-offWHite">
-                  <p>Download on the</p>
-                  <p>Appstore</p>
+                <div className="flex items-center gap-6 bg-white shadow-2xl py-3 px-3 rounded-lg">
+                  <div className="flex items-center justify-center">
+                    <img src="/assets/ellipse.png" alt="" />
+                    <img src="/assets/apple.png" alt="" className="absolute"/>
+                  </div>
+                  <div className="">
+                    <p className="font-medium text-xs">Download on the</p>
+                    <p className="text-lg font-bold">Appstore</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <div>
-                <div>
-                  <img src="" alt="" />
-                  <img src="" alt="" />
+
+              <div className="flex items-center gap-6 font-sansbg-white shadow-2xl px-3 rounded-lg">
+                <div className="flex items-center justify-center">
+                  <img src="/assets/ellipse.png" alt="" />
+                  <img src="/assets/googleplay.png" className="absolute w-4" alt="" />
                 </div>
                 <div>
-                  <p>Get on</p>
-                  <p>Google Play</p>
+                  <p className="text-xs">Get on</p>
+                  <p className="text-darkBrown font-bold">Google Play</p>
                 </div>
               </div>
             </div>
